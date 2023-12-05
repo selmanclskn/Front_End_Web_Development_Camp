e1c6c314b3370b8ba7204873e876a7f95f5cc9bc
selmanclskn <selman.clskn@hotmail.com>
2023-12-05 17:02:25 +0300

    front-end camp (24/90)

--- javascript/01-JS-Fundamentals-Part-I/script.js
+++ javascript/01-JS-Fundamentals-Part-I/script.js
@@ -314,4 +314,90 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive..");
 }
+
+
+const day = "friday";
+
+switch (day) {
+  case "monday": // day === "monday"
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day!");
+}
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
+
+
+// LECTURE: The switch statement
+const language = "türkçe";
+
+switch (language) {
+  case "çince":
+  case "mandarin":
+    console.log("çok sayıda ana dili olarak konuşan insan var");
+    break;
+  case "türkçe":
+    console.log("4.sıra");
+    break;
+  case "ispanyolca":
+    console.log("2.sıra");
+    break;
+  case "almanca":
+    console.log("5.sıra");
+    break;
+}
+
+
+const age = 27;
+age >= 18
+  ? console.log("I like to drink wine")
+  : console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
 */
