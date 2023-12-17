58455105c7fa3307a96d77ba7ab3730d3ced3b5a
selmanclskn <selman.clskn@hotmail.com>
2023-12-17 19:16:23 +0300

    front-end camp (31/90)

--- javascript/02-JS-Fundamentals-Part-II/script.js
+++ javascript/02-JS-Fundamentals-Part-II/script.js
@@ -102,4 +102,40 @@ const percentageOfWorld3 = population => {
 
 const percantageofTurkey = percentageOfWorld3(85000000);
 console.log(percantageofTurkey);
+
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired.`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1996, "Selman"));
+console.log(yearsUntilRetirement(1960, "Jonas"));
 */
