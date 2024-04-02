4fa9c721291acb0b3bc2b3374169024658899da2
selmanclskn <selman.clskn@hotmail.com>
2024-04-02 20:09:36 +0300

    prettier setting_2

--- javascript/03-Developer-Skills/script.js
+++ javascript/03-Developer-Skills/script.js
@@ -144,17 +144,17 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 // - Add ... between elements and start and end of string
 // - Log string to console
 
-const data1 = [17, 21, 23];
-const data2 = [12, 5, -5, 0, 4];
+const data1 = [17, 21, 23]
+const data2 = [12, 5, -5, 0, 4]
 
-console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`)
 
 const printForecast = function (arr) {
-  let str = '';
+  let str = ''
   for (let i = 0; i < arr.length; i++) {
-    str += `${arr[i]}ºC in ${i + 1} days ... `;
+    str += `${arr[i]}ºC in ${i + 1} days ... `
   }
-  console.log('...' + str);
-};
-printForecast(data1);
+  console.log('...' + str)
+}
+printForecast(data1)
 */
